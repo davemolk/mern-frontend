db364d976904b972de56c1a8172dfe62c11147df
davemolk <davemolk@gmail.com>
2021-06-08 10:07:12 -0600

    utils

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -1,4 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 // This utility will add the authorized user's JWT to the request header
 // Any routes that are protected will require the JWT in order to access them.
+
+const setAuthToken = (token) => {
+  if (token) {
+    // apply the token to every request header
+    axios.defaults.headers.common["Authorization"] = token;
+  } else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
+};
+
+export default setAuthToken;
